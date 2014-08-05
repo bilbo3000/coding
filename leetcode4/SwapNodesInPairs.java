b742c0c36b153a256a963713bb1a9c5d51996364
Dongpu Jin <jindongpu@gmail.com>
2014-08-05 10:21:16 -0500

    Create SwapNodesInPairs.java

---
+++ leetcode4/SwapNodesInPairs.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head; 
+        }
+        
+        ListNode temp = swapPairs(head.next.next);
+        ListNode next = head.next; 
+        head.next = temp; 
+        next.next = head; 
+        head = next; 
+        
+        return head; 
+    }
+}
