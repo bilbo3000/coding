816320672c2737db52fe3bfeb8a0f93f6adaec6c
Dongpu Jin <jindongpu@gmail.com>
2014-08-05 09:17:20 -0500

    Create RemoveElement.java

---
+++ leetcode4/RemoveElement.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int removeElement(int[] A, int elem) {
+        int len = A.length; 
+        
+        for (int i = 0; i < len; i++) {
+            if (A[i] == elem) {
+                A[i] = A[len - 1]; 
+                len--; 
+                i--; 
+            }
+        }
+        
+        return len; 
+    }
+}
