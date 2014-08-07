87cb617380473234df02100d18db1fb0c23a78f2
Dongpu Jin <jindongpu@gmail.com>
2014-08-07 14:56:23 -0500

    Create JumpGame.java

---
+++ leetcode4/JumpGame.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public boolean canJump(int[] A) {
+        int len = A.length; 
+        if (len == 0) {
+            return false; 
+        }
+        
+        boolean[] T = new boolean[len]; 
+        T[0] = true; 
+        
+        for (int i = 1; i < len; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (T[j] && A[j] >= i - j) {
+                    T[i] = true; 
+                    break; 
+                } else {
+                    T[i] = false; 
+                }
+            }
+        }
+        
+        return T[len - 1]; 
+    }
+}
