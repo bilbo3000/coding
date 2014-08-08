de46b026066117b0d94bb185e59190483a0a151d
Dongpu Jin <jindongpu@gmail.com>
2014-08-08 16:35:17 -0500

    Create Candy.java

---
+++ leetcode4/Candy.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public int candy(int[] ratings) {
+        int len = ratings.length; 
+        int[] candies = new int[len];
+        
+        for (int i = 0; i < len; i++) {
+            candies[i] = 1; 
+        }
+        
+        for (int i = 1; i < len; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1; 
+            }
+        }
+        
+        for (int i = len - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1] && candies[i] <= candies[i + 1]) {
+                candies[i] = candies[i + 1] + 1; 
+            }
+        }
+        
+        int result = 0; 
+        
+        for (int i = 0; i < len; i++) {
+            result += candies[i]; 
+        }
+        
+        return result; 
+    }
+}
