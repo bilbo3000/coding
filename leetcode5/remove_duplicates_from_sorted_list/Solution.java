be36427945b4675c4802dcaf47a73737cda9cb8e
Dongpu Jin <jindongpu@gmail.com>
2014-09-24 19:38:56 -0700

    Create Solution.java

---
+++ leetcode5/remove_duplicates_from_sorted_list/Solution.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode curr = head; 
+        
+        while (curr != null) {
+            if (curr.next != null && curr.val == curr.next.val) {
+                curr.next = curr.next.next; 
+            } else {
+                curr = curr.next; 
+            }
+        }
+        
+        return head; 
+    }
+}
