f9bbd36909e3dfa98788f15aad778fa5b7ef188b
Dongpu Jin <jindongpu@gmail.com>
2015-01-02 14:50:06 -0800

    Create Solution.java

---
+++ hackerrank/counting_sort_1/Solution.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in); 
+        int n = in.nextInt(); 
+        int[] arr = new int[100]; 
+        
+        for (int i = 0; i < n; i++) {
+            int digit = in.nextInt(); 
+            arr[digit]++; 
+        }
+        
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]); 
+            System.out.print(" "); 
+        }
+    }
+}
