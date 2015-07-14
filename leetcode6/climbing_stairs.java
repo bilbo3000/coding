42ff28650e516aa5bf3cb3e1b26aab25dd4b8921
Dongpu Jin <jindongpu@gmail.com>
2015-07-13 22:23:57 -0700

    Create climbing_stairs.java

---
+++ leetcode6/climbing_stairs.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int climbStairs(int n) {
+        if (n == 0 || n == 1 || n == 2) {
+            return n; 
+        }
+        
+        int[] T = new int[n]; 
+        T[0] = 1; 
+        T[1] = 2; 
+        
+        for (int i = 2; i < n; i++) {
+            T[i] = T[i - 1] + T[i - 2]; 
+        }
+        
+        return T[n - 1]; 
+    }
+}
