4cb0cbf131827cf71b23997c7aec88e18624eedc
bilbo3000 <bilbo3000@users.noreply.github.com>
2015-09-24 22:17:07 -0700

    Create linked_list_cycle.java

---
+++ lintcode/linked_list_cycle.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */ 
+public class Solution {
+    /**
+     * @param head: The first node of linked list.
+     * @return: True if it has a cycle, or false
+     */
+    public boolean hasCycle(ListNode head) {  
+        // write your code here
+        if (head == null) {
+            return false; 
+        }
+        
+        ListNode slow = head; 
+        ListNode fast = head.next; 
+        
+        while (fast != null) {
+            fast = fast.next; 
+            
+            if (fast != null) {
+                slow = slow.next; 
+                fast = fast.next; 
+            }
+            
+            if (slow == fast) {
+                return true; 
+            }
+        }
+        
+        return false; 
+    }
+}
+
