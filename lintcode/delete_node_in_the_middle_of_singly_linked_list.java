ef7698c25db2e63b874bb321a52332e4678a4f44
bilbo3000 <bilbo3000@users.noreply.github.com>
2015-09-27 21:36:24 -0700

    Create delete_node_in_the_middle_of_singly_linked_list.java

---
+++ lintcode/delete_node_in_the_middle_of_singly_linked_list.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ * Copy the date over from next node. Then remove next node. 
+ * O(1)
+ */ 
+public class Solution {
+    /**
+     * @param node: the node in the list should be deleted
+     * @return: nothing
+     */
+    public void deleteNode(ListNode node) {
+        if (node == null || node.next == null) {
+            return; 
+        } 
+        
+        node.val = node.next.val; 
+        node.next = node.next.next; 
+    }
+}
