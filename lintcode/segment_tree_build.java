c11d33ec5846fb19052dc0a8216b93e849e773bf
bilbo3000 <bilbo3000@users.noreply.github.com>
2015-09-29 20:02:53 -0700

    Create segment_tree_build.java

---
+++ lintcode/segment_tree_build.java
@@ -0,0 +1,35 @@
+/**
+ * Definition of SegmentTreeNode:
+ * public class SegmentTreeNode {
+ *     public int start, end;
+ *     public SegmentTreeNode left, right;
+ *     public SegmentTreeNode(int start, int end) {
+ *         this.start = start, this.end = end;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     *@param start, end: Denote an segment / interval
+     *@return: The root of Segment Tree
+     * 
+     * O(n).
+     */
+    public SegmentTreeNode build(int start, int end) {
+        if (start > end) {
+            return null; 
+        }
+
+        SegmentTreeNode node = new SegmentTreeNode(start, end);
+        
+        if (start == end) {
+            return node; 
+        }
+        
+        node.left = build(start, (start + end) / 2);
+        node.right = build((start + end) / 2 + 1, end);
+        
+        return node; 
+    }
+}
