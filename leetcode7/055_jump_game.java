521f39e96a2da21a5034f8b94424a27463ec39ec
bilbo3000 <bilbo3000@users.noreply.github.com>
2017-10-22 16:20:42 -0700

    Create 055_jump_game.java

---
+++ leetcode7/055_jump_game.java
@@ -0,0 +1,23 @@
+/*
+Determine if possible to jump to last position. 
+
+Keep track of the current left-most good position.
+If i + nums[i] >= pos, then i becomes the new pos. 
+
+In the end, if final left most good position is 0, then 
+it is reachable to the end. 
+
+Time: O(n)
+Space: O(1)
+*/
+public class Solution {
+    public boolean canJump(int[] nums) {
+        int lastPos = nums.length - 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (i + nums[i] >= lastPos) {
+                lastPos = i;
+            }
+        }
+        return lastPos == 0;
+    }
+}
